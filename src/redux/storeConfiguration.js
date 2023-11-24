c0df3567dcdc51dc463f4239f64fe4b3852cf4f1
OlehPerlovych <perlovich92@gmail.com>
2023-11-24 11:33:18 +0200

    Add logger and thunk from libraries.

--- src/redux/storeConfiguration.js
+++ src/redux/storeConfiguration.js
@@ -1,9 +1,9 @@
 import accountReducer from "./accountReducer";
 import {applyMiddleware, createStore} from "redux";
-import {loggerEnhancer} from "./enhancers/loggerEnhancer";
-import {thunkEnhancer} from "./enhancers/thunkEnhancer";
+import thunk from "redux-thunk";
+import {logger} from "redux-logger";
 
 const initialState = {balance: 10000 , quote: 'We will win!'};
-const store = createStore(accountReducer,initialState, applyMiddleware(thunkEnhancer, loggerEnhancer));
+const store = createStore(accountReducer,initialState, applyMiddleware(thunk, logger));
 
 export default store;
