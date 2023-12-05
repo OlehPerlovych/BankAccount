9593645d81e9fa92501e60d89eb49ffae70bbda2
OlehPerlovych <perlovich92@gmail.com>
2023-12-05 19:57:29 +0200

    Map object notation

--- src/redux/reducers/balanceReducer.js
+++ src/redux/reducers/balanceReducer.js
@@ -1,19 +1,36 @@
 import {depositAction, withdrawAction} from "../actions/accountActions";
+import {createReducer} from "@reduxjs/toolkit";
+import balance from "../../components/Balance";
 
 const initialBalance = 10000;
 
-function balanceReducer(balance = initialBalance, action)
-{
-    switch (action.type)
+//Map object notation
+export const balanceReducer = createReducer(initialBalance,
     {
-        case depositAction.type:
-            return balance + action.payload;
-        case withdrawAction.type:
-            const res = balance - action.payload;
-            return res<0 ? balance : res;
-        default:
-            return balance;
-    }
-}
+        [depositAction] : (state, action) =>
+        {
+            state.balance += action.payload;
+        },
+        [withdrawAction] : (state, action) =>
+        {
+            const res = state.balance - action.payload;
+            state.balance = res < 0 ? state.balance : res;
+        }
+    });
 
-export default balanceReducer;
+//
+// function balanceReducerRedux(balance = initialBalance, action)
+// {
+//     switch (action.type)
+//     {
+//         case depositAction.type:
+//             return balance + action.payload;
+//         case withdrawAction.type:
+//             const res = balance - action.payload;
+//             return res<0 ? balance : res;
+//         default:
+//             return balance;
+//     }
+// }
+//
+// export default balanceReducer;
